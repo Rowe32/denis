0d5f61666890931284374bf064be531d5809a934
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-12-13 20:58:32 +0100

    margin left on top navbar

--- src/customHooks/useOnScreen.js
+++ src/customHooks/useOnScreen.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 
 export const useOnScreen = (ref) => {
-  //const threeBreakpointThreshold = [0.25, 0.5, 0.75] // less sensitive
-  const tenBreakpointThreshold = [
-    0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
-  ];
-
   const [isOnScreen, setOnScreen] = useState(false);
   //IntersectionObserver Web API used to create observer
   //observer "notifies" if target intersects with field of view
@@ -14,7 +9,7 @@ export const useOnScreen = (ref) => {
   const observer = new IntersectionObserver(
     ([entry]) => setOnScreen(entry.isIntersecting),
     {
-      threshold: tenBreakpointThreshold,
+      threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
     }
   );
 
